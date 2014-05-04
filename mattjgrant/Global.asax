8a656ea8ed79bf3cd9381085033fa8a4b457ed82
spiralout618 <spiralout1.618@gmail.com>
2014-05-04 17:24:19 +1200

    Initial commit of default mvc5 code

---
+++ mattjgrant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mattjgrant.MvcApplication" Language="C#" %>
---
+++ mattjgrant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mattjgrant.Startup))]
+namespace mattjgrant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
